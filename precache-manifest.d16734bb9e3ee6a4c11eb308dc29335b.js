a8535d5454706edd31978e303b7b9b5780592311
srinathsr <srsrinath97@gmail.com>
2020-04-15 15:23:51 +0530

    Updates

---
+++ precache-manifest.d16734bb9e3ee6a4c11eb308dc29335b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b4f08969554ea005522a374f972318",
+    "url": "/ubereats/index.html"
+  },
+  {
+    "revision": "be709047d87368818dab",
+    "url": "/ubereats/static/css/main.3d087e9c.chunk.css"
+  },
+  {
+    "revision": "a6225d213b7c6352e34c",
+    "url": "/ubereats/static/js/2.99ca8f46.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ubereats/static/js/2.99ca8f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be709047d87368818dab",
+    "url": "/ubereats/static/js/main.34f4d323.chunk.js"
+  },
+  {
+    "revision": "87c22a739272b32095ce",
+    "url": "/ubereats/static/js/runtime-main.234ed3de.js"
+  },
+  {
+    "revision": "96b02991100736bd3a63249fc18cd8fb",
+    "url": "/ubereats/static/media/shop.96b02991.png"
+  }
+]);
